26f3e93e07a5b79fd28f99ea1a19ff8106ecf3f2
HongKong <97036597+hongkong3@users.noreply.github.com>
2026-02-15 21:23:13 +0900

    Add files via upload

---
+++ .junk/_checks.js
@@ -0,0 +1,56 @@
+﻿/** 開発用: 確認関数 - 260215 *****************
+
+- _promise(p0, p1,...), _p(p0, p1,...);
+  DevToolsでの {Promise}/Proxy(Function) 値確認用
+
+- _checkTCC();
+  全TabControlの情報を出力
+
+- _checkTC([ TC | TC.Id ]);
+  カレント/指定TCの情報を出力
+
+- _checkFV([ FV | FV.Id ]);
+  カレント/指定FVの情報を出力
+
+_check***系は実行後, `window.$p` に対象代入
+
+**********************************************/
+
+(function(){
+  window.log=window.log ? log : window.chrome ? console.log : window.alert;
+
+  window._promise = window._pa = function(){ //[Blink]: {Promise}/Proxy(Function) 実体確認用
+    Promise.all(arguments).then(window.log); return '';
+  };
+
+  window._checkTCC = function(){ //全TCちぇっく ()
+    (async function(){
+      var tcc=te.Ctrls(CTRL_TC), len=await tcc.Count, atc=[];
+      for(var i=len; i--;){
+        await Promise.all([tcc[i].Data.Group, tcc[i].Id, tcc[i].SelectedIndex, tcc[i].Selected.Id, tcc[i].Left, tcc[i].Top, tcc[i].Width, tcc[i].Height, tcc[i].Visible, tcc[i].Selected.Title, tcc[i].Count, te.Ctrl(CTRL_FV).Id]).then(function(r){
+          atc.unshift((r[11]==r[3] ? '*' : !r[0] ? '/' : r[8] ? ' ' : '-')+'['+r[0]+'-'+r[1]+'.'+r[2]+'/'+r[10]+'] ('+r[4]+', '+r[5]+', '+r[6]+', '+r[7]+') '+r[3]+':"'+r[9]+'"');
+        });
+      }
+      window.log(atc.join(',\r\n')); window.$p=tcc;
+    })(); return '';
+  };
+
+  window._checkTC = function(tc){ //TCちぇっく ([TC|TC.Id])
+    tc=typeof(tc)=='number' ? te.Ctrl(CTRL_TC, tc) : tc||te.Ctrl(CTRL_TC);
+    Promise.all([tc.Data.Group, tc.Id, tc.SelectedIndex, tc.Count, tc.Left, tc.Top, tc.Width, tc.Height, tc.Visible, te.Ctrl(CTRL_TC).Id]).then(async function(r){
+      var atc=(r[1]==r[9] ? '*' : !r[0] ? '/' : r[8] ? ' ' : '-')+'['+r[0]+'-TC_'+r[1]+'.'+r[2]+'/'+r[3]+'] ('+r[4]+', '+r[5]+', '+r[6]+', '+r[7]+')\r\nFV:[', afv=[];
+      for(var i=r[3]; i--;){await Promise.all([tc.Item(i).Id, tc.Item(i).Title]).then(function(f){afv.unshift((i==r[2] ? '*' : '')+f[0]+':"'+f[1]+'"')})}
+      window.log(atc+afv.join(', ')+']'); window.$p=tc;
+    }); return '';
+  };
+
+  window._checkFV = function(fv){ //FVちぇっく ([FV|FV.Id])
+    fv=typeof(fv)=='number' ? te.Ctrl(CTRL_FV, fv) : fv||te.Ctrl(CTRL_FV);
+    Promise.all([fv.Parent.Data.Group, fv.Parent.Id, fv.Parent.SelectedIndex, fv.Parent.Count, fv.Parent.Visible, te.Ctrl(CTRL_TC).Id, fv.Id, fv.Title, fv.History.Index, fv.History.Count]).then(async function(r){
+      var afv=(r[1]==r[5] ? '*' : !r[0] ? '/' : r[4] ? ' ' : '-')+'['+r[0]+'-'+r[1]+'.'+r[2]+'/'+r[3]+'] FV_'+r[6]+':"'+r[7]+'"\r\nHistory:[', ah=[];
+      for(var i=r[9]; i--;){await Promise.all([fv.History[i].Path, fv.History[i].Name]).then(function(h){ah.unshift((i==r[8] ? '*' : '')+i+':"'+h[0]+'"')})}
+      window.log(afv+ah.join(', ')+']'); window.$p=fv;
+    }); return '';
+  };
+})();
+
---
+++ .junk/_style.js
@@ -0,0 +1,91 @@
+﻿/** CSSによるUI装飾用のclass仕込みスクリプト - 260212
+
+## ペイン (分割画面, TabControl)
+  `div#Panel > table.layout.fixed[id^="Panel_"]`
+  - .activepane
+    アクティブなペインにクラス付与
+    UI上のTCやinner***はその配下なので、アクティブ状態でスタイル切り替えが可能
+    TreeView, FolderViewはWindowsコントロールなのでCSSじゃ無理
+  - data-group = 1～
+    所属タブグループのIDを設置, 0:未所属
+
+## フィルターバー
+  `input[type="text"][name^="filter"]`
+  - .filtered
+    フィルターが有効ならクラス付与, 色変えて目立たせたり
+
+## チェックボックス/ラジオ
+  `input[type="checkbox"].chkBase` + `span.chkCheck`, `input[type="radio"].chkBase` + `span.chkRadio`
+  CSSで描画しやすくするための仕込み
+  `.chkBase {opacity: 0}` して .chkCheck/.chkRadio で描く方式
+  `.chkBase:checked + .chkChek {...}` 等でチェック状態反映
+
+
+若干切り替えラグあるのは, 本体側のjs書き換えない限りどーにもなんない...
+**********************************************/
+
+(function(){
+  if(Addons._preparationCSS){return}
+  Addons._preparationCSS = true;
+
+  var _GA=function(e, a){return e.getAttribute(a)}, _SA=function(e, a, v){return e.setAttribute(a, v)},
+    _CE=function(t, c, v, o){var e=document.createElement(t||'div'); o=o||{}; if(c){e.className=c} if(v){e.innerHTML=v} for(var a in o){_SA(e, a, o[a])} return e}
+
+  async function _classPane(Ctrl){ // アクティブペイン(分割画面): .activepane  ('div#Panel > table.layout.fixed[id^="Panel_"]')
+    var tid=await Ctrl.Parent.Id, tcc=await te.Ctrls(CTRL_TC), tcg=[0], l=await tcc.Count;
+    var att=document.querySelectorAll('#Panel > table[id^="Panel_"]'), tt=document.getElementById('Panel_'+tid);
+    while(l--){var nid=await tcc[l].Id; tcg[nid]=await tcc[l].Data.Group}
+    for(var i=att.length; i--;){
+      att[i].classList[att[i]==tt ? 'add' : 'remove']('activepane');
+      _SA(att[i], 'data-group', tcg[att[i].id.replace(/^.*?(\d+)$/, '$1')||0]);
+    }
+  }
+
+  function _classFilter(){ // 有効フィルタバー(input): .filterd  ('input[type="text"][name^="filter"]')
+    var af=document.querySelectorAll('input[type="text"][name^="filter"]');
+    for(var i=af.length; i--;){af[i].classList[/[^\*]/.test(af[i].value) ? 'add' : 'remove']('filtered')}
+  }
+
+  function _classCheckRadio(){ // 装飾用にチェック/ラジオのDOM構造改変: .chkBase .chkCheck/.chkRadio (.btnChkSet)
+    var ac=document.querySelectorAll('input[type="checkbox"]:not(.chkBase), input[type="radio"]:not(.chkBase)');
+    for(var i=ac.length; i--;){
+      var c=ac[i], cs=getComputedStyle(c), l;
+      c.parentNode.insertBefore((l=_CE('label')), c).appendChild(c).classList.add('chkBase');
+      if(cs.display=='none' || cs.visibility=='hidden'){continue}
+      l.appendChild(_CE('span', c.type=='radio' ? 'chkRadio' : 'chkCheck'));
+
+      // アドオン一覧等では, なぜか切り替え毎に作り直しなので...
+      if(/^_\w+|^enable_/.test(c.id)){c.addEventListener('change', function(){setTimeout(_classCheckRadio, 50)})}
+    }
+    if(ac.length){setTimeout(_classCheckRadio, 150)}
+
+    ac=document.querySelectorAll('input[type="button"]:not(.btnChkSet), button:not(.btnChkSet)'); //動的追加対策
+    for(var i=ac.length; i--;){ac[i].addEventListener('click', function(){setTimeout(_classCheckRadio, 30)}); ac[i].classList.add('btnChkSet')}
+  }
+
+
+  AddEvent("ChangeView1", function(Ctrl){ // ev:FVフォーカス変更
+    _classPane(Ctrl);
+    _classFilter(); for(var i=16; i>>=1;){setTimeout(_classFilter, i*60)}
+  });
+
+  AddEvent("FilterChanged", function(Ctrl){ // ev:フィルター変更
+    _classFilter();
+    for(var i=16; i>>=1;){setTimeout(_classFilter, i*60)}
+  });
+
+  AddEvent("Load", function(){ // ev:起動後
+    _classFilter();
+  });
+
+  AddEvent("BrowserCreatedEx", async function(){ // ev:TEウィンドウ生成後
+    if($.chrome){setTimeout(function(){document.body.classList.add('wb_blink')}, 100)}
+    _classCheckRadio(); setTimeout(_classCheckRadio, 100);
+  }.toString().replace(/^([\s\S]+)$/, '($1)();'));
+
+  // TE本体だけは "BrowserCreatedEx" 経過(というかコレ)なので直接実行
+  if($.chrome){setTimeout(function(){document.body.classList.add('wb_blink')}, 100)}
+  setTimeout(_classCheckRadio, 100);
+
+})();
+
